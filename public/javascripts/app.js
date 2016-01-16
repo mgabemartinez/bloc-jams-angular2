9633b06db05309bed7a28983a5885b15fc0e682a
Gabe Martinez <mgabemartinez@gmail.com>
2016-01-15 21:38:31 -0800

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -328,10 +328,33 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require("./profile");
+//require('./landing');
+ //require('./album');
+ //require('./collection');
+ //require('./profile');
+
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  console.log("Landing.controller");
+  $scope.subText = "turn the music up";
+
+  $scope.subTextClicked = function() {
+     $scope.subText += '!';
+   };
+
+
+    $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+ }])
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
