450ba58eba40318b933e85335be5fd9e328d96b8
Gabe Martinez <mgabemartinez@gmail.com>
2016-03-30 13:29:12 -0700

    Built a basic slider directive

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -411,7 +411,6 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
 
 // Landing Controller
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
-  console.log("Landing.controller");
   $scope.subText = "turn the music up";
 
   $scope.subTextClicked = function() {
@@ -572,11 +571,57 @@ blocJams.service('SongPlayer', function() {
       this.play();
     }
   }
-})
+});
+
+
+ blocJams.directive('slider', function(){
+
+  var updateSeekPercentage = function($seekBar, event) {
+     var barWidth = $seekBar.width();
+     var offsetX =  event.pageX - $seekBar.offset().left;
+ 
+     var offsetXPercent = (offsetX  / $seekBar.width()) * 100;
+     offsetXPercent = Math.max(0, offsetXPercent);
+     offsetXPercent = Math.min(100, offsetXPercent);
+ 
+     var percentageString = offsetXPercent + '%';
+     $seekBar.find('.fill').width(percentageString);
+     $seekBar.find('.thumb').css({left: percentageString});
+   }
 
 
 
 
+   return {
+     templateUrl: '/templates/directives/slider.html',
+     replace: true,
+     restrict: 'E',
+     link: function(scope, element, attributes) {
+ 
+      var $seekBar = $(element);
+ 
+      $seekBar.click(function(event) {
+        updateSeekPercentage($seekBar, event);
+      });
+ 
+      $seekBar.find('.thumb').mousedown(function(event){
+        $seekBar.addClass('no-animate');
+ 
+        $(document).bind('mousemove.thumb', function(event){
+          updateSeekPercentage($seekBar, event);
+        });
+ 
+        //cleanup
+        $(document).bind('mouseup.thumb', function(){
+          $seekBar.removeClass('no-animate');
+          $(document).unbind('mousemove.thumb');
+          $(document).unbind('mouseup.thumb');
+        });
+ 
+      });
+    }
+   };
+ });
 
 
 
